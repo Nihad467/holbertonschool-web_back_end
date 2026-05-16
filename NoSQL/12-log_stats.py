85bd9cdba2f87a2f884554e6c050afe4ef79ee84
Nihad467 <nihadseferzade01@gmail.com>
2026-05-16 21:54:54 +0400

    Fix MongoDB log stats script

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-"""Script that displays statistics about Nginx logs in MongoDB."""
+"""Script that provides statistics about Nginx logs."""
 
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
-    collection = client.logs.nginx
+    logs_collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
-    print("{} logs".format(total_logs))
+    logs_count = logs_collection.count_documents({})
+    print("{} logs".format(logs_count))
     print("Methods:")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        method_count = logs_collection.count_documents(
+            {"method": {"$eq": method}}
+        )
+        print("\tmethod {}: {}".format(method, method_count))
 
-    status_count = collection.count_documents(
-        {"method": "GET", "path": "/status"}
+    status_count = logs_collection.count_documents(
+        {
+            "$and": [
+                {"method": {"$eq": "GET"}},
+                {"path": "/status"}
+            ]
+        }
     )
     print("{} status check".format(status_count))
